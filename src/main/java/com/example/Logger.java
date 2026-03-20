df206b433515f9b174a4195bb8a918d53c07b328
SemyonKim <59174164+SemyonKim@users.noreply.github.com>
2026-03-20 18:32:27 +0500

    Update Logger.java

--- src/main/java/com/example/Logger.java
+++ src/main/java/com/example/Logger.java
@@ -2,6 +2,15 @@ package com.example;
 
 public class Logger {
 
+    private Logger() {}
+
+    private static class loggerHolder{
+        private static final Logger INSTANCE = new Logger();
+    }
+
+    public static Logger getInstance(){
+        return loggerHolder.INSTANCE;
+    }
 
     void log(String logMessage) {
         System.out.println(logMessage);
