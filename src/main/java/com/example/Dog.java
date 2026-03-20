20974418a68f4a28e6a3b2833345625ebc16c0aa
SemyonKim <59174164+SemyonKim@users.noreply.github.com>
2026-03-20 18:32:08 +0500

    Update Dog.java

--- src/main/java/com/example/Dog.java
+++ src/main/java/com/example/Dog.java
@@ -2,12 +2,13 @@ package com.example;
 
 public class Dog {
 
-    private final Logger logger = new Logger();
+    private final Logger logger = Logger.getInstance();
 
     public void woof() {
         logger.log("Woof");
     }
 
-
-
+    public Logger getLogger(){
+        return logger;
+    }
 }
